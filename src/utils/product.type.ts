81ce4e2a29a95ec24d11e4a6ba9615c678dad1b6
Surya Bumantara <bumantara.surya2213@gmail.com>
2023-11-29 21:39:43 +0700

    add static serve image & typedefs

---
+++ src/utils/product.type.ts
@@ -0,0 +1,33 @@
+import { Product } from '@entity/Product.entity'
+import { Field, InputType, ObjectType } from 'type-graphql'
+
+@ObjectType()
+class ProductList {
+  @Field()
+  count!: number
+  @Field(() => [Product])
+  products!: Product[]
+  @Field(() => Number)
+  price_min!: number
+  @Field(() => Number)
+  price_max!: number
+}
+
+@InputType()
+class filterProducts {
+  @Field(() => Number, {nullable: true})
+  price_min?: number | null
+  @Field(() => Number, {nullable: true})
+  price_max?: number | null
+  @Field(() => [Number], {nullable: true})
+  category_ids?: []
+  @Field(() => [Number], {nullable: true})
+  material_ids?: []
+  @Field(() => String, {nullable: true})
+  search?: string | null
+}
+
+export {
+  ProductList,
+  filterProducts
+}
