33be525b2020c89c3f083667fad33d6d79fdf926
Surya Bumantara <bumantara.surya2213@gmail.com>
2024-07-10 09:36:22 +0700

    add seed course & endpoint courseDetail

--- src/resolvers/Course.resolver.ts
+++ src/resolvers/Course.resolver.ts
@@ -1,10 +1,21 @@
 import { Course } from '@entity/Course.entity'
-import { Query, Resolver } from 'type-graphql'
+import { Arg, Query, Resolver } from 'type-graphql'
 
 @Resolver(Course)
 export class CourseResolver {
   @Query(() => [Course])
   async courses(): Promise<Course[]> {
     return await Course.find()
+  }
+
+  @Query(() => Course)
+  async courseDetail(
+    @Arg('id') id: number,
+  ): Promise<Course> {
+    const course = await Course.createQueryBuilder('crs')
+      .where('crs.id = :id', { id })
+      .getOneOrFail()
+
+    return course
   }
 }
