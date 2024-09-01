5004fb08942d94cb55592790e4ce8ec95eb9b132
Surya Bumantara <bumantara.surya2213@gmail.com>
2024-09-01 21:24:58 +0700

    update SKU duplicate checker

--- src/utils/composables.ts
+++ src/utils/composables.ts
@@ -1,16 +1,18 @@
 import { Variant } from '@entity/Variant.entity'
 
-export const checkUniqueSKU = async (skus: string[]) => {
-  const sameSKU = await Variant.createQueryBuilder('variant')
+export const checkUniqueSKU = async (skus: string[], current_product_id?: number) => {
+  console.log(skus)
+  const query = Variant.createQueryBuilder('variant')
     .where('variant.sku IN (:...skus)', { skus })
-    .getMany()
+  
+  current_product_id && query.andWhere('variant.product != :product', { product: current_product_id })
+
+  const sameSKU = await query.getMany()
+  console.log(sameSKU)
 
   const unique = new Set(skus)
   const isUnique = unique.size === skus.length
 
-  if (sameSKU.length > 0 || !isUnique) {
-    return false
-  }
-
-  return true
+  if (sameSKU.length > 0 || !isUnique) return false
+  else return true
 }
