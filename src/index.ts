2e4c42b37aab462fe1315f7e5a394bd633397e68
Surya Bumantara <bumantara.surya2213@gmail.com>
2024-09-28 15:42:45 +0700

    add package helmet

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,7 @@ import { readFileSync } from 'fs'
 import { CourseTransactionResolver } from '@resolver/CourseTransaction.resolver'
 import { ComplaintResolver } from '@resolver/Complaint.resolver'
 import { checkCustomTransactionStatusCronJob } from '@utils/cron'
+import helmet from 'helmet'
 dotenv.config()
 
 const main = async () => {
@@ -38,6 +39,8 @@ const main = async () => {
   app.use(cors({
     origin: '*', // or use "*" for all origins
   }))
+
+  app.use(helmet({crossOriginResourcePolicy: {policy: 'cross-origin'}}))
 
   app.use(express.static('public'))
   app.use(graphqlUploadExpress())
